26e741580207484e3047585e22fe1d086fd1cf37
Lhaura <laurarodriguez154@yahoo.com>
2022-09-28 15:19:06 -0400

    Final commits

--- README.md
+++ README.md
@@ -15,9 +15,11 @@ To develop and application named Note Taker that can be use to create and save n
 
 
 ## Website
-
+ https://lhaurod.github.io/note-taker/
 
 Screenshots
+<img width="1332" alt="Screen Shot 2022-09-28 at 3 07 52 PM" src="https://user-images.githubusercontent.com/106787987/192869708-968e002a-005f-488b-bfd6-c4b479490a75.png">
+<img width="1312" alt="Screen Shot 2022-09-28 at 3 15 33 PM" src="https://user-images.githubusercontent.com/106787987/192869716-f8209585-6bba-45b3-b218-c58241cb08dd.png">
 
 
 ## Contribution
--- server.js
+++ server.js
@@ -1,86 +1,77 @@
-// Dependencies
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
-const util = require("util");
+const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const path = require('path');
 
-
-// Handling Async Processes
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
-
-
-// Server setup
+const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3002;
+
+const allNotes = require('./db/db.json');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-
-// Static Middleware
-app.use(express.static("public"));
-
-
-// API  "GET" request
-app.get("/api/notes", function(req, res) {
-  readFileAsync("db/db.json", "utf8").then(function(data) {
-      notes = [].concat(JSON.parse(data))
-      res.json(notes);
-    })
-}); 
-
-
-// API  "POST" request
-app.post("/api/notes", function(req, res) {
-    const theNote = req.body;
-    readFileAsync("db/db.json", "utf8").then(function(data) {
-      const theNotes = [].concat(JSON.parse(data));
-      theNote.id = theNotes.length + 1
-      theNotes.push(theNote);
-      return theNotes
-    }).then(function(notes) {
-      writeFileAsync("db/db.json", JSON.stringify(notes))
-      res.json(theNote);
-    })
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
-// API "DELETE" request
-app.delete("/api/notes/:id", function(req, res) {
-  const deleteID = parseInt(req.params.id);
-  readFileAsync("db/db.json", "utf8").then(function(data) {
-    const notes = [].concat(JSON.parse(data));
-    const newNotes = []
-    for (let i = 0; i<notes.length; i++) {
-      if(deleteID !== notes[i].id) {
-        newNotes.push(notes[i])
-      }
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+function createNewNote(body, notesArray) {
+    const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return newNote;
+}
+
+app.post('/api/notes', (req, res) => {
+    const newNote = createNewNote(req.body, allNotes);
+    res.json(newNote);
+});
+
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
+
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
+
+            break;
+        }
     }
-    return newNotes
-  }).then(function(notes) {
-    writeFileAsync("db/db.json", JSON.stringify(notes))
-    res.send('saved success!!!');
-  })
-})
+}
 
-
-// HTML Routes
-app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, allNotes);
+    res.json(true);
 });
 
-app.get("/", function(req, res) {
-     res.sendFile(path.join(__dirname, "/public/index.html"));
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
-
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-
-
-// Listening
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-});
-
